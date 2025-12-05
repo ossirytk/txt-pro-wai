fbd7619b230d47c62c47ab248b5af002d1441323
ossirytk <ossirytk@gmail.com>
2025-12-05 18:17:50 +0200

    Starting subs work

--- py_tesst.py
+++
@@ -1,3 +0,0 @@
-import pytesseract
-text = pytesseract.image_to_string(Image.open("processed_image.png"))
-print(text)
---
+++ subs_test.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+from loguru import logger
+from icecream import ic
+
+logger.remove(0)
+logger.add(sys.stdout, format="{level} {message}", level="INFO")
+
+def main() -> None:
+    logger.info("Processing subs")
+    subs = Path() / "src_subs" / "HDMs3e1.srt"
+    logger.debug("Reading subs from {subs}")
+    with subs.open() as sub_file:
+        raw_subtitles = sub_file.read()
+        logger.debug("Read subs")
+
+    ic(len(raw_subtitles))
+
+    subtitle_items = raw_subtitles.split("\n\n")
+    ic(len(subtitle_items))
+
+    logger.info("Done")
+
+if __name__ == "__main__":
+    main()
